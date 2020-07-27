b760d18a70fb1b6df525ca2150b22a014c2542eb
farisachugthai <farischugthai@gmail.com>
2020-07-26 21:52:38 -0400

    Gitignore and chars of different sizes

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,10 @@ remember that `!` means that it is a macro.
 
 fn main() {
     println!("Hello, world number {}!", 8);
+
+    // All chars are 4 bytes. They are 4 bytes because some characters in a string are more than one byte. For example:
+    let slice = "Hello!";
+    println!("Slice is {:?} bytes.", std::mem::size_of_val(slice)); // std::mem::size_of_val gives the size in bytes
+    let slice2 = "안녕!"; // Korean for "hi"
+    println!("Slice2 is {:?} bytes.", std::mem::size_of_val(slice2));
 }
